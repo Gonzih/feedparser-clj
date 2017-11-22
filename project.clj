687df30de477d1cace5c4057e1a952c5b48a871b
Max Gonzih <gonzih@gmail.com>
2017-11-22 14:16:51 +0100

    Remove not used dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject org.clojars.gnzh/feedparser-clj "0.6.0"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.9.0-RC1"]
-                 [org.jdom/jdom2 "2.0.6"]
                  [net.java.dev.rome/rome "1.0.0"]])
