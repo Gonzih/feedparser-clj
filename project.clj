8f5dd90526e5e87cc973b19985819548053bcd2a
Max Gonzih <gonzih@gmail.com>
2017-12-08 23:29:34 +0100

    Bump dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.gnzh/feedparser-clj "0.6.0"
+(defproject org.clojars.gnzh/feedparser-clj "0.6.1"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [net.java.dev.rome/rome "1.0.0"]])
